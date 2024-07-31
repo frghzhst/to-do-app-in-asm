95651d750b899bf53a78079e70536c9e0a272b3f
carson <carsonisalsome@gmail.com>
2024-07-31 17:59:35 +0800

    made file read idk

--- source.asm
+++ source.asm
@@ -1,9 +1,39 @@
+section .data
+    filename db 'logs.txt', 0
+    buffer db 1024 dup(0)   
+
 section .text
     global _start
+
 _start:
+    jmp startup
+startup:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, 0x77656c636f6d6520746f206d7920746f2d646f206170700a
+    mov edx, 24
+    int 0x80
+    
+    ;jmp read
+read:
+    mov eax, 5         
+    mov ebx, filename  
+    mov ecx, 0         
+    int 0x80           
+    
+    test eax, eax      
+    js file_not_found  
 
-logs:
+    mov edx, eax      
 
-printandread:
+    mov eax, 3         
+    mov ebx, edx      
+    mov ecx, buffer    
+    mov edx, 1024      
+    int 0x80           
 
-section .data
+    mov eax, 6         
+    mov ebx, edx      
+    int 0x80          
+
+file_not_found:
