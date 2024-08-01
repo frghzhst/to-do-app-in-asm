85198f544e8b4444a86259336935ac335970ac09
carson <carsonisalsome@gmail.com>
2024-08-01 19:42:12 +0800

    made stuff, not assembled yet

--- source.asm
+++ source.asm
@@ -15,6 +15,12 @@ section .data
     sizeexit equ - exitmsg
     newline db 0x0a, 0
     lsize equ - newline
+    writing db 'what do you want to add', 0x0a, 0
+    wl equ - writing
+    removerin db 'what do you want to remove', 0x0a, 0
+    rsize equ - removerin
+    nein db 'no match of what you wanted to remove', 0x0a, 0
+    neinsize equ - nein
 
 section .bss
     buf resb 255
@@ -56,6 +62,7 @@ startup:
     mov ecx, buf  ; buffer being the input
     mov edx, 255     
     int 0x80 ; read user input
+    xor ecx, ecx
 
     ;mov byte [ecx + eax], 0 
     
@@ -78,6 +85,7 @@ startup:
     je exit
 
 read:
+    xor buf, buf
     mov eax, 5
     mov ebx, filename
     mov ecx, 0
@@ -105,11 +113,96 @@ read:
     jmp startup
 
 write:
-    jmp exit
+    xor buf, buf
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, writing
+    mov edx, wl
+    int 0x80
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, buf
+    mov edx, 255
+    int 0x80
+    push buf
+
+    mov esi, user_input   ; Point to the start of user input
+    xor ecx, ecx          ; Initialize counter for size calculation
+
+count_loop:
+    cmp byte [esi], 0     ; Check for null terminator or newline character
+    je end_count          ; If termination character found, end counting
+    inc ecx               ; Increment counter
+    inc esi               ; Move to the next byte
+    jmp count_loop        ; Repeat until termination character is found
+end_count:
+    mov eax, 4
+    mov ebx, [fd_in]
+    mov ecx, buf
+    mov edx, ecx
+    int 0x80
+    jmp startup
 
 remove:
-    jmp exit
+    xor buf, buf
+     mov eax, 5
+    mov ebx, filename
+    mov ecx, 0
+    mov edx, 0777
+    int 0x80 ; opens file for read
 
+    mov [fd_in], eax ; save the file descriptor for later use
+
+    mov eax, 3
+    mov ebx, [fd_in]
+    mov ecx, info
+    mov edx, 26
+    int 0x80 ; reads from the file
+
+    mov eax, 6
+    mov ebx, [fd_in]
+    int 0x80 ; close file
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, info
+    mov edx, 26
+    int 0x80 ; prints the file
+
+    mov ecx, removerin
+    mov edx, rsize
+    int 0x80
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, buf
+    mov edx, 255
+    int 0x80
+
+    mov esi, buf
+    mov edi, info
+loop:
+    mov al, [esi]
+    mov bl, [edi]
+    cmp al, bl
+    jne ne
+    cmp al, 0
+    je eq 
+    inc esi
+    inc edi
+    jmp loop
+
+ne:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nein
+    mov edx, neinsize
+    int 0x80
+    jmp startup
+
+eq:
+    ;logic for removing the todo from logs
 exit:
 
     mov eax, 4
